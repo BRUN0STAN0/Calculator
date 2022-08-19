9cf73db54c598a2cee5db6cfa77077bbf5a0ffab
Bruno <109989747+BRUN0STAN0@users.noreply.github.com>
2022-08-19 11:19:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let display = document.getElementById("inputext");
+
+let calculate = (number) => {
+    display.value += number;
+}
+
+let result = () => {
+    try {
+        display.value = eval(display.value)
+    }
+    catch(err) {
+        alert(display.value+" -> IT'S NOT A VALID INPUT");
+    }
+}
+
+function c() {
+    display.value = "";
+}
+
+function ce(){
+    display.value = display.value.slice(0,-1)
+}
+
+function sqrt () {
+    display.value = Math.sqrt(display.value);
+}
